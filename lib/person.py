2e8faec1e74f4171dcdff791c6bf1810f81bbea8
Mutindi Hope <msndindi@outlook.com>
2023-05-21 18:48:37 +0300

    Completed Assignment

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+
 class Person:
     # Class body goes here
 
     #Instance method definition
     pass
+    def walk(self):
+        print("The person is walking.")
+
+    def talk(self):
+        print("Hello World!")
